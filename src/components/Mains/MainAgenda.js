d21669e41b579c4937fce7c6407056b7d5c94117
MeridjaNassim <ha_meridja@esi.dz>
2020-02-24 20:23:02 +0100

    Agenda started

---
+++ src/components/Mains/MainAgenda.js
@@ -0,0 +1,5 @@
+import React from "react"
+import { MainWrapper as Wrapper } from "../Container"
+export default function MainAgenda() {
+  return <Wrapper></Wrapper>
+}
--- src/pages/agenda.js
+++ src/pages/agenda.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
-import MainConstruction from "../components/layout/MainConstruction"
+import Main from "../components/Mains/MainAgenda"
 // TODO : Implement Page
 // TODO : Add Agenda to the page ( the actual agenda)
 export default function agenda() {
   return (
     <Layout>
       <SEO title="Agenda"></SEO>
-      <MainConstruction title="Agenda"></MainConstruction>
+      <Main></Main>
     </Layout>
   )
 }
